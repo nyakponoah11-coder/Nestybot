eb13e46bd0d0e9843db9cf39d7be88ddb81ef691
nyakponoah11-coder <nyakponoah11@gmail.com>
2026-08-10 09:55:27 +0000

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ approve manually.
 
 const NETFLIX_EMAIL = process.env.NETFLIX_EMAIL;
 const NETFLIX_EMAIL_APP_PASSWORD = process.env.NETFLIX_EMAIL_APP_PASSWORD;
-const NETFLIX_PRICE = 1;
+const NETFLIX_PRICE = 30;
 
 /* =========================================================
 PACKAGES
